d7942236f1760c87b15234c3611f5bc5210936fd
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-27 14:46:19 -0500

    1.21c revised

--- Program.cs
+++ Program.cs
@@ -160,3 +160,6 @@ Console.Clear();
 // List the character(s) with no alias (all series) - return character name, alias and series only.
 Console.WriteLine("Characters with no alias (all series):");
 foreach (var character in characters.Where(c => c.Alias.Count == 0).Select(c => new { c.Name, c.Alias, c.Series }))
+{
+    Console.WriteLine($"\t{character.Name} - Alias: [{string.Join(", ", character.Alias)}] - Series: {string.Join(", ", character.Series)}");
+}
