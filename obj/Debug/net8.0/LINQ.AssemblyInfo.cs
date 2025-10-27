b08dc51bbf66d9c39745a68b61e942b99c88afbb
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-27 14:00:37 -0500

    define character class / create list of characters from json data

--- obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb181ed9116c33cfc1b921b04ca2d396b76150b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8971adf81ec81f0772543ac25f49a6bbe1532c16")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
