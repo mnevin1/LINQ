52a878532234f9977b4bbbb972a71c0e07ec55fb
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-27 14:32:06 -0500

    character(s) created in 1981 all series

--- Program.cs
+++ Program.cs
@@ -95,12 +95,16 @@ Console.Clear();
 // }
 
 // all characters grouped by year created
-var CharactersByYearCreated = characters.GroupBy(c => c.YearCreated);
-foreach (var characterByYearCreated in CharactersByYearCreated)
-{
-    Console.WriteLine(characterByYearCreated.Key);
-    foreach (var character in characterByYearCreated)
-    {
-        Console.WriteLine($"\t{character.Name}");
-    }
-}
+// var CharactersByYearCreated = characters.GroupBy(c => c.YearCreated);
+// foreach (var characterByYearCreated in CharactersByYearCreated)
+// {
+//     Console.WriteLine(characterByYearCreated.Key);
+//     foreach (var character in characterByYearCreated)
+//     {
+//         Console.WriteLine($"\t{character.Name}");
+//     }
+// }
+
+// How many character(s) were created in 1981 (all series)?
+int charactersCreatedIn1981 = characters.Count(c => c.YearCreated == 1981);
+Console.WriteLine($"[1.19a] Characters created in 1981 (all series): {charactersCreatedIn1981}");
