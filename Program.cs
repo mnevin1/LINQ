d8d390d97765bb7169ba4893b604670650e3b070
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-27 14:26:17 -0500

    display which characters have the longest name

--- Program.cs
+++ Program.cs
@@ -85,5 +85,11 @@ Console.Clear();
 //   Console.WriteLine($"{obj.Name} has {obj.Alias.Count()} alias(s):\n\t{String.Join(", ", obj.Alias)}");
 // }
 
+// how many letters make up the longest character name(s)
 int LengthOfName = characters.Max(c => c.Name!.Length);
 Console.WriteLine($"There are {characters.Max(c => c.Name!.Length)} letters in the longest character's name");
+// which characters have the longest name?
+foreach(string? name in characters.Where(c => c.Name!.Length == LengthOfName).Select(c => c.Name))
+{
+  Console.WriteLine($"\t{name}");
+}
