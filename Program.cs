2c8e0d6f0ad6bacd4489f3a99ced41e58d157c24
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-27 14:10:01 -0500

    display which characters were created in 1995

--- Program.cs
+++ Program.cs
@@ -54,3 +54,8 @@ bool Character1995 = characters.Any(c => c.YearCreated == 1995);
 Console.WriteLine($"Are there characters created in 1995: {Character1995}");
 // how many characters were created in 1995
 Console.WriteLine($"How many? {characters.Count(c => c.YearCreated == 1995)}");
+// which characters were created in 1995
+foreach(Character character in characters.Where(c => c.YearCreated == 1995))
+{
+  Console.WriteLine(character.Display());
+}
